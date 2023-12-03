440a7c4f6a20d79073545e8b922c0780a2e18398
Zago1986 <guilhermezago1986@hotmail.com>
2023-12-03 20:41:01 -0300

    Criada a classe Posicao

--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Tabuleiro;
+
+namespace ChessGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Posicao p = new Posicao(3, 4);
+            Console.WriteLine("Posicao: " + p);
+        }
+    }
+}
---
+++ ChessGame/Tabuleiro/Posicao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tabuleiro
+{
+    internal class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Linha);
+            sb.Append(", ");
+            sb.Append(Coluna);
+            return sb.ToString();
+        }
+    }
+}
