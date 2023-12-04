22beb8b98f5d552b7d478ff05d954c923f02da10
Zago1986 <guilhermezago1986@hotmail.com>
2023-12-03 21:46:18 -0300

    Colocando pecas no tabuleiro

---
+++ ChessGame/Chess/Rei.cs
@@ -0,0 +1,16 @@
+﻿using Board;
+
+namespace Chess
+{
+    internal class Rei : Peca
+    {
+        public Rei(Cor cor, Tabuleiro tabuleiro) : base(cor, tabuleiro)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "R";
+        }
+    }
+}
--- ChessGame/Program.cs
+++ ChessGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Board;
+using Chess;
 
 namespace ChessGame
 {
@@ -6,9 +7,13 @@ namespace ChessGame
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
-            Tela.ImprimirTabuleiro(tab);
+            tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(0, 0));
+            tabuleiro.ColocarPeca(new Torre(Cor.Preta, tabuleiro), new Posicao(1, 3));
+            tabuleiro.ColocarPeca(new Rei(Cor.Preta, tabuleiro), new Posicao(2, 4));
+
+            Tela.ImprimirTabuleiro(tabuleiro);
         }
     }
 }
